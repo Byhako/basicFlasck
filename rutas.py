481fc584cd97ef870d0183a4c2b7fa32abb395d1
Ruben E Acosta <eldragondera1@gmail.com>
2019-10-04 17:07:13 -0500

    rutas

--- rutas.py
+++ rutas.py
@@ -13,5 +13,15 @@ def saludo():
     param = request.args.get('nombre', 'desconocido')
     return '<h2>Hola {}</h2>'.format(param)
 
+# /cursos/fisica/logica
+@app.route('/cursos/<curso1>/<curso2>')
+def cursos(curso1, curso2):
+    return '<h2>Selene estudia {} y {}</h2>'.format(curso1, curso2)
+
+# /libros/2
+@app.route('/libros/<int:id>/') # valido que sea numerico el parametro
+def libros(id):
+    return '<h2>Selene lee el libro {}</h2>'.format(id)
+
 if __name__ == '__main__':
-    app.run(debug=True, port=3000)
+    app.run(debug=True, port=5001)
