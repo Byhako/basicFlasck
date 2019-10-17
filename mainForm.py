29086f85acdbf239297ccf05a1b836ab170f6212
Ruben E Acosta <eldragondera1@gmail.com>
2019-10-17 15:18:27 -0500

    validating data form

--- mainForm.py
+++ mainForm.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import formulario
 
 app = Flask(__name__)
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-  comment_form = formulario.CommentForm()
-  return render_template('form.html', form=comment_form )
+    comment_form = formulario.CommentForm(request.form)
+    print('hola')
+    if request.method == 'POST':
+        print('username: ', comment_form.username.data)
+        print('email: ', comment_form.email.data)
+        print('comment: ', comment_form.comment.data)
+
+    return render_template('form.html', form=comment_form)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5001)
